ed3949ee18d58695a015009d7b0c2025ea3a03a8
Alexis Woodward <alexis.woodward1717@outlook.com>
2021-01-05 10:05:48 -0600

    initial commit

---
+++ FSWDFinalProject/FSWDFinalProject.UI.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FSWDFinalProject.UI.MVC.MvcApplication" Language="C#" %>
---
+++ FSWDFinalProject/FSWDFinalProject.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FSWDFinalProject.UI.MVC.Startup))]
+namespace FSWDFinalProject.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
